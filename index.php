3f06e7a37f5027bf530a1eb52434fd88bb46a796
Shajibul Alam Shihab <82690571+shihab-2021@users.noreply.github.com>
2022-04-11 08:30:11 +0600

    php practic code

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    echo 'Hello World!' . '<br>';
+    echo '<br>';
+
+    $number1 = 10;
+    $number2 = 9.40;
+
+    $country = "Bangladesh";
+
+    echo "First number is: $number1 and Second number is: $number2 <br>";
+    echo "Country name is: $country <br>";
+
+    echo "<br> ------------- Array ------------- <br><br>";
+
+    $arr = array(5, 4, 2, 1, 3);
+    array_push($arr, 6);
+
+    echo "Normal output";
+    for($i=0; $i<=5; $i++)
+    {
+        echo $arr[$i] . '<br>';
+    }
+
+    echo "<br> Sorted output with foreach loop <br>";
+    sort($arr);
+    foreach($arr as $value)
+    {
+        echo $value . '<br>';
+    }
+
+?>
